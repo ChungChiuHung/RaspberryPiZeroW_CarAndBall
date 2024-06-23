978b92c54c265956aec5518688f70183c021e1eb
ChiuHungChung <NSDrum@hotmail.com>
2024-06-23 19:17:33 +0800

    update

--- src/distance.py
+++ src/distance.py
@@ -3,6 +3,8 @@ import time # Import time library
 import aiohttp # Import aiohttp library
 import asyncio # Import asyncio library
 
+print("aiohttp version: ", aiohttp.__version__)
+
 GPIO.setmode(GPIO.BCM) # Set GPIO pi number
 
 TRIG = 15 # Associate pin 15 to TRIG
@@ -16,14 +18,16 @@ GPIO.setup(ECHO,GPIO.IN)
 out_of_range_time = None
 passed_time = 600 # 10 minutes
 
-url_ball_1 = "http://192.168.1.11"
-url_ball_2 = "http://192.168.1.12"
-url_ball_3 = "http://192.168.1.13"
-url_ball_4 = "http://192.168.1.14"
-url_ball_5 = "http://192.168.1.15"
-url_ball_6 = "http://192.168.1.16"
-url_ball_7 = "http://192.168.1.17"
-url_ball_8 = "http://192.168.1.18"
+urls = [
+    "http://192.168.1.11",
+    "http://192.168.1.12",
+    "http://192.168.1.13",
+    "http://192.168.1.14",
+    "http://192.168.1.15",
+    "http://192.168.1.16",
+    "http://192.168.1.17",
+    "http://192.168.1.18"
+]
 
 ball_go = "on"
 ball_stop = "off"
@@ -33,6 +37,10 @@ async def make_request(url, state):
     async with aiohttp.ClientSession() as session:
         async with session.get(request_url) as resp:
             print(f"{request_url} Response: {resp.status}")
+
+async def handle_requests(state):
+    tasks = [make_request(url, state) for url in urls]
+    await asyncio.gather(*tasks)
 
 while True:
     GPIO.output(TRIG, False)
@@ -50,7 +58,6 @@ while True:
         pulse_end = time.time()
 
     pulse_duration = pulse_end - pulse_start
-
     distance = pulse_duration * 17150
     distance = round(distance, 2)
 
@@ -58,15 +65,7 @@ while True:
         print ("Distance: ",distance - 0.5, "cm")
         # request balls to move
         # Send the HTTP request
-        asyncio.run(make_request(url_ball_1, ball_go))
-        asyncio.run(make_request(url_ball_2, ball_go))
-        asyncio.run(make_request(url_ball_3, ball_go))
-        asyncio.run(make_request(url_ball_4, ball_go))
-        asyncio.run(make_request(url_ball_5, ball_go))
-        asyncio.run(make_request(url_ball_6, ball_go))
-        asyncio.run(make_request(url_ball_7, ball_go))
-        asyncio.run(make_request(url_ball_8, ball_go))
-
+        asyncio.run(handle_requests(ball_go))
         out_of_range_time = None # Reset the out of range time
     else:
         print ("Out of Range") # display out of range
@@ -74,13 +73,5 @@ while True:
             out_of_range_time = time.time() # Record the time when it went out of range
         elif time.time() - out_of_range_time >= passed_time:
             # Send the HTTP request to balls to stop
-            asyncio.run(make_request(url_ball_1, ball_stop))
-            asyncio.run(make_request(url_ball_2, ball_stop))
-            asyncio.run(make_request(url_ball_3, ball_stop))
-            asyncio.run(make_request(url_ball_4, ball_stop))
-            asyncio.run(make_request(url_ball_5, ball_stop))
-            asyncio.run(make_request(url_ball_6, ball_stop))
-            asyncio.run(make_request(url_ball_7, ball_stop))
-            asyncio.run(make_request(url_ball_8, ball_stop))
-
+            asyncio.run(handle_requests(ball_stop))
             out_of_range_time = time.time()
