9f1859f185541d82d8e38a6b9492e8c4c8439e83
ChiuHungChung <nsdrum@hotmail.com>
2024-06-28 16:49:00 +0800

    update

--- src/distance.py
+++ src/distance.py
@@ -2,7 +2,7 @@ import RPi.GPIO as GPIO # Import GPIO library
 import time # Import time library
 import aiohttp # Import aiohttp library
 import asyncio # Import asyncio library
-import aiohttp as web # Import web server from aiohttp
+from aiohttp import web # Import web server from aiohttp
 
 print("aiohttp version: ", aiohttp.__version__)
 
